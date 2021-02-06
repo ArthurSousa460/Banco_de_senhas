1425d671a0b41fe9e0a6b9823ec9e6a16b531755
ArthurSousa460 <arthurprogrammer460@gmail.com>
2021-02-06 10:14:04 -0300

    primeira_alteração
    mudança do Readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Banco_de_senhas
- Pequena aplicação que salva o site e a senha do ususario.
+ -Pequena aplicação que salva o site e a senha do ususario.
+ -Feito com Tkinter e sqlite3 
---
+++ main.py
@@ -0,0 +1,90 @@
+from tkinter import *
+from tkinter import ttk
+import sqlite3
+
+class aplication(object):
+    def __init__(self, tk):
+
+        self.label1 = Label(tk, text='Formulario de senhas', background='#dde')
+        self.label1.place(x=80, y= 10)
+
+        self.label2= Label(tk, text='Site', background='#dde', foreground='#009', anchor='w')
+        self.label2.place(x=30, y=75 , width=100, height=20)
+
+        self.site = Entry(tk)
+        self.site.place(x=70, y=75, width= 200, height=20)
+
+        self.label3 = Label(tk, text='Senha', background='#dde', foreground='#009', anchor='w')
+        self.label3.place(x=25, y=100, width=100, height=20)
+   
+        self.senha = Entry(tk)
+        self.senha.place(x=70, y=100, width= 200, height=20)
+
+        self.botao = Button(tk, text='salvar', command=self.write)
+        self.botao.place(x=220, y=130, width=40, height=30)
+
+        self.botao1 = Button(tk, text='Banco de senhas', command=self.page_two)
+        self.botao1.place(x=120, y=130, width=95, height=30)
+
+        self.data = sqlite3.connect('banco_de_senha.db')
+        self.cursor = self.data.cursor()
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS Cadastro(Site TEXT NOT NULL, Senha INTEGER NOT NULL) ''')
+
+
+    def write(self):
+        site_text = self.site.get()
+        senha_text = self.senha.get()
+        if site_text != '':
+            self.cursor.execute('''INSERT into Cadastro(Site, Senha) VALUES(?, ?)''', (site_text, senha_text))
+            self.data.commit()
+            self.data.close()
+            self.site.delete(0, END)
+            self.senha.delete(0, END)
+
+
+    def page_two(self):
+        tk = Tk()
+        tk.geometry('350x263')
+        tk.title('lista de senhas')
+        tk.configure(background='#dde')
+        self.table = ttk.Treeview(tk, columns=('Site', 'Senha'), show='headings')
+        self.table.column('Site', minwidth=0, width=150)
+        self.table.column('Senha', minwidth=0)
+        self.table.heading('Site', text='Site')
+        self.table.heading('Senha', text='Senha')
+        self.table.pack()
+        botao = Button(tk, text='deletar', command=self.delete)
+        botao.place(x=300, y=230, width=40, height=30)
+ 
+        self.data = sqlite3.connect('banco_de_senha.db')
+        self.cursor = self.data.cursor()
+        self.cursor.execute("SELECT * FROM Cadastro")
+        rows = self.cursor.fetchall()
+        for row in rows:
+            self.table.insert("", END, values=row)
+            self.data.close()
+
+        tk.mainloop()
+
+
+    def delete(self):
+        select_item = self.table.selection()
+        self.data = sqlite3.connect('banco_de_senha.db')
+        self.cursor = self.data.cursor()
+        self.table.delete(select_item)
+        self.cursor.execute('''DELETE FROM Cadastro WHERE Site=? AND Senha=?''')
+        self.data.commit()
+        self.data.close()
+    
+
+
+tk = Tk()
+aplication(tk)
+
+tk.title('Banco de senhas')
+
+tk.geometry('290x200')
+
+tk.configure(background='#dde')
+
+tk.mainloop()
